52964aa31796671fe0b2500fe00e51ce1c6bd01b
EarthY.Y <supanat5947@gmail.com>
2024-01-24 17:43:04 +0700

    finish demo
    รัน เเชทบอลได้

--- model/chat.py
+++ model/chat.py
@@ -8,8 +8,9 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('database/intents.json', 'r') as json_data:
     intents = json.load(json_data)
+
 
 FILE = "data.pth"
 data = torch.load(FILE)
@@ -20,6 +21,8 @@ output_size = data["output_size"]
 all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
+
+
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
