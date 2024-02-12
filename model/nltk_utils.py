c0f7ead5a7a0ce561b45cc59a36fefec1be7c14e
EarthY.Y <supanat5947@gmail.com>
2024-02-12 08:14:53 +0700

    12/02/67 8.14
    อัปเดท Json traindata รองรับภาษาไทย เพิ่ม Design หน้าเว็บ

--- model/nltk_utils.py
+++ model/nltk_utils.py
@@ -1,19 +1,17 @@
 import numpy as np
 import nltk
 #nltk.download('punkt')
-import os
+from pythainlp import word_tokenize
 
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
     # sentence =' '.join(sentence) #ถ้าทำการเเยกคำต้องนี้จะเป็นการเเยกตัวอักษรเลย เเต่ก็ได้ผลที่ดีมาก
-    sentence = sentence.encode('utf-8').decode('utf-8')
-    return nltk.word_tokenize(sentence)
+    return word_tokenize(sentence)
 
 
 def stem(word):
-    word = word.encode('utf-8').decode('utf-8')
     return stemmer.stem(word.lower())
 
 
@@ -24,7 +22,6 @@ def bag_of_words(tokenized_sentence, words):
     # initialize bag with 0 for each word
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
-        w = w.encode('utf-8').decode('utf-8')
         if w in sentence_words: 
             bag[idx] = 1
 
